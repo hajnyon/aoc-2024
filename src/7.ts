f2759514744239877fa5d8bdefd96b77d3d76348
Ondřej Hajný <hajnyon@gmail.com>
2024-12-07 14:35:29 +0100

    feat: 7-1

---
+++ src/7.ts
@@ -0,0 +1,28 @@
+export default (lines: string[]) => {
+	let result = 0;
+
+	const solve = (nums: number[], test: number, index: number, acc = 0) => {
+		if (index === nums.length) {
+			return {
+				isCorrect: test === acc,
+				acc,
+			};
+		}
+		const addRes = solve(nums, test, index + 1, acc + nums[index]);
+		const multRes = solve(nums, test, index + 1, acc * nums[index]);
+		return addRes.isCorrect ? addRes : multRes;
+	};
+
+	for (const line of lines) {
+		const parsed = line.split(": ");
+		const test = Number(parsed[0]);
+		const nums = parsed[1].split(" ").map((num) => Number(num));
+
+		const res = solve(nums, test, 1, nums[0]);
+		if (res.isCorrect) {
+			result += res.acc;
+		}
+	}
+
+	return result;
+};
