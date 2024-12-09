9ab878eea12bbf85099a6df4cda420c81cd7a50c
Ondřej Hajný <hajnyon@gmail.com>
2024-12-09 12:01:45 +0100

    feat: 9-2

--- src/9.ts
+++ src/9.ts
@@ -1,7 +1,7 @@
 export default (lines: string[]) => {
 	const disk = lines[0].split("");
 
-	const diskData: number[] = [];
+	const diskData: (number | null)[] = [];
 	let fileIndex = 0;
 	for (const [index, element] of disk.entries()) {
 		const num = Number.parseInt(element);
@@ -15,25 +15,57 @@ export default (lines: string[]) => {
 		}
 	}
 
-	let startIndex = 0;
-	let endIndex = diskData.length - 1;
-	while (startIndex < endIndex) {
-		if (diskData[startIndex] !== null) {
-			startIndex++;
-			continue;
+	const findFreeSlotIndex = (size: number, maxIndex: number): number | null => {
+		let acc = 0;
+		let index = 0;
+		while (true) {
+			if (diskData[index] !== null) {
+				index++;
+				acc = 0;
+				continue;
+			}
+			acc++;
+			if (acc === size && index < maxIndex) {
+				return index - size + 1;
+			}
+			index++;
+			if (index >= maxIndex) {
+				return null;
+			}
 		}
-		if (diskData[endIndex] === null) {
-			endIndex--;
+	};
+
+	for (let i = diskData.length - 1; i > 0; i--) {
+		// skip free slots
+		if (diskData[i] === null) {
 			continue;
 		}
 
-		// switch two
-		const temp = diskData[startIndex];
-		diskData[startIndex] = diskData[endIndex];
-		diskData[endIndex] = temp;
-		startIndex++;
-		endIndex--;
+		// buffer all file data
+		const buffer: (number | null)[] = [];
+		while (true) {
+			buffer.push(diskData[i]);
+			if (diskData[i - 1] !== buffer[0]) {
+				break;
+			}
+			i--;
+		}
+
+		// find the first free slot
+		const freeSlotIndex = findFreeSlotIndex(buffer.length, i);
+		if (freeSlotIndex === null) {
+			continue;
+		}
+
+		// switch buffer with free slot
+		for (const [index, b] of buffer.entries()) {
+			diskData[freeSlotIndex + index] = b;
+			diskData[i + index] = null;
+		}
 	}
 
-	return diskData.reduce((acc, cur, index) => acc + cur * index, 0);
+	return diskData.reduce(
+		(acc, cur, index) => (cur !== null ? acc + cur * index : acc),
+		0,
+	);
 };
