c65d2d1039ab5f0061e11de12269ab769e1c1d8f
Ondřej Hajný <hajnyon@gmail.com>
2024-12-13 12:19:31 +0100

    feat: 11-2

--- src/11.ts
+++ src/11.ts
@@ -1,32 +1,31 @@
 export default (lines: string[]) => {
-	let stones = lines.map((line) =>
-		line.split(" ").map((char) => Number(char)),
-	)[0];
+	let stones = new Map<bigint, bigint>();
+	for (const char of lines[0].split(" ")) {
+		stones.set(BigInt(char), 1n);
+	}
 
-	let blinkCount = 25;
+	let blinkCount = 75;
 	while (blinkCount > 0) {
-		const newStones: number[] = [];
-		for (let i = 0; i < stones.length; i++) {
-			const stone = stones[i];
+		const newStones = new Map<bigint, bigint>();
+		for (const [stone, count] of stones.entries()) {
 			const stoneStr = stone.toString();
-			if (stone === 0) {
-				newStones.push(1);
-				continue;
+			if (stone === 0n) {
+				newStones.set(1n, (newStones.get(1n) ?? 0n) + count);
+			} else if (stoneStr.length % 2 === 0) {
+				const split1 = BigInt(stoneStr.slice(0, stoneStr.length / 2));
+				const split2 = BigInt(stoneStr.slice(stoneStr.length / 2));
+				newStones.set(split1, (newStones.get(split1) ?? 0n) + count);
+				newStones.set(split2, (newStones.get(split2) ?? 0n) + count);
+			} else {
+				const newStone = BigInt(stone * 2024n);
+				newStones.set(newStone, (newStones.get(newStone) ?? 0n) + count);
 			}
-			if (stoneStr.length % 2 === 0) {
-				const split1 = stoneStr.slice(0, stoneStr.length / 2);
-				const split2 = stoneStr.slice(stoneStr.length / 2);
-				newStones.push(Number(split1));
-				newStones.push(Number(split2));
-				continue;
-			}
-			newStones.push(stone * 2024);
 		}
-		stones = [...newStones];
+		stones = newStones;
 		blinkCount--;
 	}
 
-	const result = stones.length;
+	const result = [...stones.values()].reduce((a, b) => a + b, 0n);
 
 	return result;
 };
