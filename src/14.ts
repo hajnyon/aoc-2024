33edf62eede33f474f281e63821f7fd84e8a49d8
Ondřej Hajný <hajnyon@gmail.com>
2024-12-15 13:01:41 +0100

    feat: 14-2

--- src/14.ts
+++ src/14.ts
@@ -1,51 +1,36 @@
 export default (lines: string[]) => {
 	const COLS = 101;
 	const ROWS = 103;
-	const SECS = 100;
-	const colsMiddle = Math.ceil((COLS - 1) / 2);
-	const rowsMiddle = Math.ceil((ROWS - 1) / 2);
-	const quadrants = {
-		topLeft: 0,
-		topRight: 0,
-		bottomLeft: 0,
-		bottomRight: 0,
-	};
+	const SECS = 10000;
 
-	for (const line of lines) {
-		const lineMatch = line.match(/p=(\d+)\,(\d+)\sv=(-?\d+),(-?\d+)/);
-		const pX = Number(lineMatch?.[1]);
-		const pY = Number(lineMatch?.[2]);
-		let vX = Number(lineMatch?.[3]);
-		// transform velocity to positive value
-		if (vX < 0) {
-			vX = vX + COLS;
-		}
-		let vY = Number(lineMatch?.[4]);
-		if (vY < 0) {
-			vY = vY + ROWS;
-		}
-		const posX = (vX * SECS + pX) % COLS;
-		const posY = (vY * SECS + pY) % ROWS;
+	for (let sec = 0; sec <= SECS; sec++) {
+		const map: number[][] = Array.from({ length: ROWS }, () =>
+			new Array(COLS).fill(0),
+		);
+		for (const line of lines) {
+			const lineMatch = line.match(/p=(\d+)\,(\d+)\sv=(-?\d+),(-?\d+)/);
+			const pX = Number(lineMatch?.[1]);
+			const pY = Number(lineMatch?.[2]);
+			let vX = Number(lineMatch?.[3]);
+			// transform velocity to positive value
+			if (vX < 0) {
+				vX = vX + COLS;
+			}
+			let vY = Number(lineMatch?.[4]);
+			if (vY < 0) {
+				vY = vY + ROWS;
+			}
+			const posX = (vX * sec + pX) % COLS;
+			const posY = (vY * sec + pY) % ROWS;
 
-		if (posX < colsMiddle && posY < rowsMiddle) {
-			quadrants.topLeft++;
+			map[posY][posX] = map[posY][posX] + 1;
 		}
-		if (posX < colsMiddle && posY > rowsMiddle) {
-			quadrants.bottomLeft++;
-		}
-		if (posX > colsMiddle && posY < rowsMiddle) {
-			quadrants.topRight++;
-		}
-		if (posX > colsMiddle && posY > rowsMiddle) {
-			quadrants.bottomRight++;
+
+		const l = map.map((r) => r.map((c) => (c > 0 ? "#" : ".")).join(""));
+		if (l.some((row) => row.includes("######"))) {
+			console.log(sec, "\n", l.join("\n"));
 		}
 	}
 
-	const result =
-		quadrants.topLeft *
-		quadrants.topRight *
-		quadrants.bottomLeft *
-		quadrants.bottomRight;
-
-	return result;
+	return 0;
 };
